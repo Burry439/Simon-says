5b3b7648c550a86199f75aa04bbd1b2a79b532c3
burry <burry439@gmail.com>
2018-01-01 15:36:28 +0200

    ff

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-// process.env.CONNECTION_STRING|| 
 
 var express = require('express');
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/simonSays', function() {
-  console.log("DB connection established!!!");
-})
+// mongoose.connect(process.env.CONNECTION_STRING|| 'mongodb://localhost/simonSays', function() {
+//   console.log("DB connection established!!!");
+// })
 var highScoreM = require('./models/model')
 
 var app = express();
@@ -18,29 +17,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 
-app.get('/scores', function(req, res){
-  highScoreM.find(function(err, result){
-    if(err){
-     throw (err)
-    }else{       
-      res.send(result)
-      }   
-  })
-})
+// app.get('/scores', function(req, res){
+//   highScoreM.find(function(err, result){
+//     if(err){
+//      throw (err)
+//     }else{       
+//       res.send(result)
+//       }   
+//   })
+// })
 
-app.put('/highScore/:id', function(req,res){
-  var highScore = {
-    name: req.body.name,
-     score: req.body.score
-  }
-  highScoreM.update({_id : req.params.id}, highScore , function(err, result){
-    if(err){
-      console.log(err)
-    }
-   console.log(result)
-    res.send("result")
-});
-})
+// app.put('/highScore/:id', function(req,res){
+//   var highScore = {
+//     name: req.body.name,
+//      score: req.body.score
+//   }
+//   highScoreM.update({_id : req.params.id}, highScore , function(err, result){
+//     if(err){
+//       console.log(err)
+//     }
+//    console.log(result)
+//     res.send("result")
+// });
+// })
 
 app.listen(process.env.PORT || 8000, function() {
     console.log("yo yo yo, on 8000!!")
